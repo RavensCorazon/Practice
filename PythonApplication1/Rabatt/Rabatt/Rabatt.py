d60581471550fee4ff3c9ba0ffcf603f2dfba648
RavensCorazon <92933450+RavensCorazon@users.noreply.github.com>
2021-10-29 15:59:20 +0200

    Update Rabatt.py

--- PythonApplication1/Rabatt/Rabatt/Rabatt.py
+++ PythonApplication1/Rabatt/Rabatt/Rabatt.py
@@ -1,8 +1,14 @@
 preis = float(input(f"Bitte geben Sie den Preis an der Rabatt bekommen soll: "))
 rabatt_prozent = float(input(f"Geben Sie den Rabatt ein: "))
+
+if rabatt_prozent > 99.99:
+    print("Mehr als 100% Rabatt nicht möglich.\n")
+    rabatt_prozent = float(input(f"Geben Sie den Rabatt ein: "))
+elif rabatt_prozent == 1:
+    print(rabatt_prozent)
+
 rabatt = float(preis) * float(rabatt_prozent) / 100
 Endpreis = float(preis) - float(rabatt)
-
 print(f"Das ist der Startpreis: {preis}")
 print(f"Darauf gibt es {rabatt_prozent}%, dies sind {rabatt}€")
 print(f"Der Endpreis beträgt: {Endpreis}")
