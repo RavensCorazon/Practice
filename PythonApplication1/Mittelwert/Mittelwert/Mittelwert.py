17bfad87e1c4b2e7c77cec6c55eefbddcd9eb3e8
RavensCorazon <92933450+RavensCorazon@users.noreply.github.com>
2021-11-11 18:39:53 +0100

    Add files via upload

---
+++ PythonApplication1/Mittelwert/Mittelwert/Mittelwert.py
@@ -0,0 +1,9 @@
+
+z_1 = int(input("Geben Sie ihre erste Zahl ein: "))
+z_2 = int(input("Geben Sie ihre zweite Zahl ein: "))
+z_3 = int(input("Geben Sie ihre dritte Zahl ein: "))
+z_4 = int(input("Geben Sie ihre vierte Zahl ein: "))
+
+mitt = z_1 + z_2 + z_3 + z_4 / 4
+
+print(f"Sie haben ein Mittelwert von {mitt}")
